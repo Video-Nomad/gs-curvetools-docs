2a3db903de01153a5c00763aa2de8a6613ecfa20
George Sladkovsky <george.sladkovsky@gmail.com>
2022-04-27 10:55:04 +0300

    version bump

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,7 @@ copyright = '%s, George Sladkovsky' % YEAR
 author = 'George Sladkovsky'
 
 # The full version, including alpha/beta/rc tags
-release = 'v1.2'
+release = 'v1.2.7'
 
 # -- General configuration ---------------------------------------------------
 
