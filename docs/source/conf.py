3765293b74f0893c515f57e6c0132a2c796a8e07
George Sladkovsky <george.sladkovsky@gmail.com>
2025-12-02 08:39:54 +0100

    v1.3.16-2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,7 @@ copyright = "%s, George Sladkovsky" % copyright_year
 author = "George Sladkovsky"
 
 # The full version, including alpha/beta/rc tags
-release = "v1.3.14"
+release = "v1.3.16"
 
 # -- General configuration ---------------------------------------------------
 
